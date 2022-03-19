2e9b1a557eb7da282f8c282aee9c788ed4390862
ayaanok <101917908+ayaanok@users.noreply.github.com>
2022-03-19 11:28:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ client.on("ready", () => {
     console.log(`[!]: Logged in as ${client.user.tag}`)
 });
 
+if (!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send("**You Dont Have Permmissions To official Someone! - [MANAGE_GUILD]**");
+if (!message.guild.me.hasPermission("MANAGE_GUILD")) return message.channel.send("**I Don't Have Permissions To official Someone! - [MANAGE_GUILD]**")
+if (!args[0]) return message.channel.send("**Please Enter A User To official**");
+
 client.on("mesageCreate", (message) => {
     if (message.content.startsWith("official")) {
         const user = message.mentions.members.first();
